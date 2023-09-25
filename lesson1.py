07e3605f4bb47bcfd078549c963beafc27b7128a
niniko02 <146019044+niniko02@users.noreply.github.com>
2023-09-25 23:16:25 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,1 @@
+print("nino lobjanidze")
---
+++ turtle_1.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+#we want to paint a house
+
+#step 1: draw a square
+
+width(7)
+color("brown")
+forward (200)
+left(90)
+
+forward (200)
+left(90)
+     
+forward (200)
+left(90)
+
+forward (200)
+left(90)
+#end of square
+
+#drawing a door
+
+forward(70)
+
+exitonclick()
+
+
+ 
+
+
